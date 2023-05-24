11fc638d048cfa48cacff0aa40017e594637712d
anderssongodoy <anderssongodoygarica@gmail.com>
2023-05-24 06:55:15 -0500

    Primer Componente

---
+++ 02-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react'
+
+export const FirstApp = () => {
+    return (
+        <Fragment>
+            <h1>Fernando</h1>
+            <p>Soy un subtitulo</p>
+        </Fragment>
+    )
+}
---
+++ 02-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+// function App(){
+//     // document.createElement....
+//     return (<h1>Hola Mundo</h1>)
+// }
+
+// export default App
+
+export const HelloWorldApp = () => {
+  return (
+    <div>HelloWorldApp</div>
+  )
+}
+
+// export const HelloWorldApp = () => <h1>Arrow Function</h1>
