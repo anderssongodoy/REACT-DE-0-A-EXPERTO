3ce855db5b5aba8a5a6d091ef408727b8b292f0a
anderssongodoy <anderssongodoygarica@gmail.com>
2023-05-24 20:52:21 -0500

    Tests en Javascript

--- 02-counter-app-vite/src/base-pruebas/11-async-await.js
+++ 02-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -1,19 +1,17 @@
 
 
-export const getImagen = async () => {
+export const getImagen = async() => {
 
     try {
         const apiKey = 'Hqf9KsV9Z41C96cx3cb4EcqIDAwJHEFm'
         const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
         const { data } = await resp.json()
 
-        const { url } = data.images.original;
+        // const { url } = data.images.original;
 
         return url
-    } catch (err) {
+    } catch (error) {
         // console.error(error)
         return 'No se encontro la imagen'
     }
 }
-
-getImagen()
